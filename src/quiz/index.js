6bf2f58fa098414303f723afd19415f032635b05
sandeepkumaar <cskumaar1992@gmail.com>
2023-08-08 02:12:39 +0530

    add other routes

--- src/quiz/index.js
+++ src/quiz/index.js
@@ -3,12 +3,13 @@ import randomiseArray from './randomise-array.js';
 import {questions} from './list.js'
 
 let storage = new Map();
+storage.set('randomQuizList', []);
 
 const router = express.Router();
 
 
-router.post(`/create`, async function(req, res, ctx) {
-  await storage.clear();
+router.post(`/create`, function(req, res, next) {
+  storage.clear();
   const {count} = req.body;
   let randomQuizList = randomiseArray(questions, Number(count));
   //randomQuizList = randomQuizList.map((q, index) => ({
@@ -16,7 +17,7 @@ router.post(`/create`, async function(req, res, ctx) {
   //  index
   //}));
 
-  await storage.set('randomQuizList', randomQuizList);
+  storage.set('randomQuizList', randomQuizList);
   let ids =  randomQuizList.map(({question_id}) => question_id);
   return res.status(200).json({
     ok: true,
@@ -25,6 +26,64 @@ router.post(`/create`, async function(req, res, ctx) {
   })
 })
 
+router.get(`/:id`, function(req, res, next) {
+  let {id} = req.params;
+  let randomQuizList = storage.get('randomQuizList');
+  let quiz = randomQuizList.find(({question_id}) => {
+    return question_id === Number(id);
+  })
 
+  res.status(200);
+  return res.json({
+    ok: true,
+    quiz
+  })
+});
+
+router.get(`/`, function(req, res, next) {
+  let randomQuizList = storage.get('randomQuizList');
+  //let values = Object.values(questionsById);
+
+  return res.status(200).json({
+    ok: true,
+    randomQuizList
+  })
+})
+
+router.put(`/`, function(req, res, next) {
+  let {id, userAnswerIndex} = req.body;
+  let randomQuizList = storage.get('randomQuizList');
+  log.debug({randomQuizList, id}, 'randomQuizList');
+  let quiz = randomQuizList.find(({question_id}) => {
+    //console.log(question_id);
+    return question_id === Number(id);
+  })
+  if(!quiz) {
+    res.status(500)
+    return res.json({
+      ok: false,
+      statusText: 'Internal Server Error',
+      message: 'Cannot find the Item to update'
+    })
+  }
+
+  
+  let isCorrect = false;
+  if(quiz.answer_index === userAnswerIndex) {
+    isCorrect = true;
+  };
+  quiz = {...quiz, isCorrect, userAnswerIndex};
+  
+  let quizIndex = quiz.index;
+  randomQuizList[quizIndex] = quiz;
+  storage.set('randomQuizList', randomQuizList);
+
+  res.status(200)
+  return res.json({
+    ok: true,
+    quiz,
+  })
+
+})
 
 export default router;
